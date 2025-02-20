b3189186befdbf4e5ae08315a6004b0059b04d31
LukasTrust <108077617+LukasTrust@users.noreply.github.com>
2025-02-20 11:23:35 +0100

    save

--- src/main/java/financialmanager/objectFolder/responseFolder/Response.java
+++ src/main/java/financialmanager/objectFolder/responseFolder/Response.java
@@ -1,22 +1,3 @@
 package financialmanager.objectFolder.responseFolder;
 
-import lombok.Getter;
-
-@Getter
-public class Response {
-    private final AlertType alertType;
-    private final String message;
-    private Object data;
-
-    public Response(AlertType alertType, String message) {
-        this.alertType = alertType;
-        this.message = message;
-    }
-
-    public Response(AlertType alertType, String message, Object data) {
-        this.alertType = alertType;
-        this.message = message;
-        this.data = data;
-    }
-
-}
+public record Response(AlertType alertType, String message, Object data) { }
