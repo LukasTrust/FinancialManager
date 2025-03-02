3ad33eeada484f24340fa159cfd856625e4ba637
LukasTrust <108077617+LukasTrust@users.noreply.github.com>
2025-03-02 20:51:06 +0100

    checked chart.ts && chart.js

--- src/main/resources/static/scripts/dataStorage.js
+++ src/main/resources/static/scripts/dataStorage.js
@@ -1,14 +1,15 @@
 const contentArea = document.getElementById('content');
-let timer;
+let timer = null;
 let alerts = [];
-let existingChart = null;
+let existingChart = null; // Consider specifying a more precise type if possible
 let bankAccountId;
-let bankAccountSymbols= {}
+let bankAccountSymbols = {};
 let currentLanguage;
-let monthAbbreviations;
-let transactionData;
-let filteredTransactionData;
-let transactionsHiddenToggle;
-let selectedTransactionGroup;
-let selectedCounterparty;
-let selectedContract;
+let monthAbbreviations = [];
+let transactionData = []; // Define a proper type if available
+let filteredTransactionData = []; // Define a proper type if available
+let transactionsHiddenToggle = false;
+let selectedTransactionGroup = null;
+let selectedCounterparty = null;
+let selectedContract = null;
+//# sourceMappingURL=dataStorage.js.map
