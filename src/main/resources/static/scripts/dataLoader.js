67debe613af4de49c5388b775024a07f6804b598
LukasTrust <108077617+LukasTrust@users.noreply.github.com>
2025-03-02 20:50:46 +0100

    checked chart.ts && chart.js

--- src/main/resources/static/scripts/dataLoader.js
+++ src/main/resources/static/scripts/dataLoader.js
@@ -1,41 +1,36 @@
 document.addEventListener('DOMContentLoaded', async () => {
     await setLocale();
-
     // Load sidebar
     await loadSidebar();
-
-    // Load fist page to view
+    // Load first page to view
     await buildAddBankAccount();
-
     // Listen for clicks on links (or buttons) to dynamically load content
     document.querySelectorAll('a[data-ajax="true"]').forEach(link => {
         link.addEventListener('click', async (event) => {
-            event.preventDefault();  // Prevent the default link behavior
-
+            var _a;
+            event.preventDefault(); // Prevent the default link behavior
             const url = link.getAttribute('href');
-
-            if (link.parentElement.id) {
-                bankAccountId = link.parentElement.id;
+            if (!url)
+                return;
+            if ((_a = link.parentElement) === null || _a === void 0 ? void 0 : _a.id) {
+                bankAccountId = Number(link.parentElement.id);
             }
-
             await loadURL(url);
         });
     });
 });
-
 async function loadURL(url) {
     try {
         // Fetch the content from the server (just the content fragment)
         const response = await fetch(url);
         const html = await response.text();
-
         // Extract and replace the content (assuming the fragment is inside a div with class "content")
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
         const newContent = doc.getElementById('content');
-
-        // Replace the old content with the new one
-        contentArea.innerHTML = newContent.innerHTML;
+        if (contentArea && newContent) {
+            contentArea.innerHTML = newContent.innerHTML;
+        }
         switch (url) {
             case "/addBankAccount":
                 await buildAddBankAccount();
@@ -49,21 +44,24 @@ async function loadURL(url) {
             case "/changeContract":
                 break;
         }
-
-    } catch (error) {
+    }
+    catch (error) {
         console.error("Error loading content:", error);
     }
 }
-
 async function loadBankAccounts() {
     try {
         const response = await fetch('/getBankAccountsOfUser');
         if (response.ok) {
             return await response.json();
-        } else {
+        }
+        else {
             console.warn('Failed to load bank accounts');
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.error("Error fetching bank accounts:", error);
     }
+    return [];
 }
+//# sourceMappingURL=dataLoader.js.map
