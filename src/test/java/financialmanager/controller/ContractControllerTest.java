bd873be7fab167a11ebdcd04330bad19fa71169c
Lukas <Lukas-Scheler@gmx.de>
2025-04-02 08:24:56 +0200

    added ContractControllerTest for integration tests

---
+++ src/test/java/financialmanager/controller/ContractControllerTest.java
@@ -0,0 +1,42 @@
+package financialmanager.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContractControllerTest {
+
+    
+
+    @Test
+    void getContractsForBankAccount() {
+    }
+
+    @Test
+    void getContractDisplaysForBankAccount() {
+    }
+
+    @Test
+    void updateContractName() {
+    }
+
+    @Test
+    void updateContractDescription() {
+    }
+
+    @Test
+    void hideContracts() {
+    }
+
+    @Test
+    void unHideContracts() {
+    }
+
+    @Test
+    void mergeContracts() {
+    }
+
+    @Test
+    void deleteContracts() {
+    }
+}
